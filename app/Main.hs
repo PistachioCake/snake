4033c3925cf3a983d9d4bebd82d41347a58c4750
PistachioCake <rushil.udani@gmail.com>
2018-08-10 09:52:15 -0500

    Add food and all related functionality

--- app/Main.hs
+++ app/Main.hs
@@ -2,39 +2,50 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
+import System.Random
 import Lib
 
+(<@>) :: (Applicative f) => f (a -> b) -> a -> f b
+f <@> a = f <*> pure a
+infixl 4 <@>
 
 main :: IO ()
-main = playIO window bkg fps initialState renderIO handleEventsIO updateIO
+main = playIO window bkg fps initialState renderIO handleEventsIO update
     where 
         window = InWindow "Test" (600, 400) (10, 10)
         bkg = black
-        fps = 2
+        fps = 5
         renderIO :: Game -> IO Picture
         renderIO g = return $ render g
         handleEventsIO :: Event -> Game -> IO Game
         handleEventsIO e g = return $ handleEvents e g
-        updateIO :: Float -> Game -> IO Game
-        updateIO f g = return $ update f g
 
 
 initialState :: Game
-initialState = Game up [Unit (0, 0)]
+initialState = Begin
 
-update :: Float -> Game -> Game
-update _ (Game vel@(velx, vely) units) = 
-    let (Unit front) = head units
-        end = init units
-    in Game vel $ Unit (velx + fst front, vely + snd front):end
+update :: Float -> Game -> IO Game
+update _ Begin = Game <$> getStdGen <@> 0 <@> up <@> Unit (0, 0) <@> [Unit (0, 0)]
+update _ (Game gen oldadd vel@(velx, vely) food snake) = 
+    let front = head snake
+        (Unit frontPos) = front
+        add = oldadd + if front == food then 3 else 0
+        end = drop 1 $ if add > 0 then init snake else snake
+    in 
+        if front == food then
+            let (x, gen') = randomR (-10, 10) gen
+                (y, gen'') = randomR (-15, 15) gen'
+            in return $ Game gen'' (oldadd + 2) vel (Unit (x, y)) $ Unit (velx + fst frontPos, vely + snd frontPos):snake
+        else
+            return $ Game gen (max 0 (oldadd - 1)) vel food $ Unit (velx + fst frontPos, vely + snd frontPos):(if oldadd > 0 then snake else init snake)
 
 handleEvents :: Event -> Game -> Game
-handleEvents (EventKey key Down _ _) (Game vel snake) = Game
-    (case key of
-        SpecialKey KeyUp -> up
-        SpecialKey KeyDown -> down
-        SpecialKey KeyLeft -> left
-        SpecialKey KeyRight -> right
-        _ -> vel)
-    snake
+handleEvents (EventKey key Down _ _) (Game gen add vel food snake) = 
+    let newvel = case key of
+            SpecialKey KeyUp -> up
+            SpecialKey KeyDown -> down
+            SpecialKey KeyLeft -> left
+            SpecialKey KeyRight -> right
+            _ -> vel
+    in Game gen add newvel food snake
 handleEvents _ game = game
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Lib
-    ( Unit (Unit), Game (Game), 
+    ( Unit (Unit), Game (Begin, Game), 
       Vel, up, down, left, right, render
     ) where
 
 import Graphics.Gloss.Data.Picture
 import Graphics.Gloss.Data.Color
+import System.Random
 
-newtype Unit = Unit (Int, Int)
+newtype Unit = Unit (Int, Int) deriving (Eq, Show)
 type Vel = (Int, Int)
-data Game = Game {velocity :: Vel, snake :: [Unit]}
+data Game = Begin | Game {gen :: StdGen, add :: Int, velocity :: Vel, food :: Unit, snake :: [Unit]}
 
 up, down, left, right :: Vel
 up = (0, 1)
@@ -20,7 +23,8 @@ class Renderable a where
     render :: a -> Picture
 
 instance Renderable Unit where
-    render (Unit (x, y)) = color green $ translate (10 + 20 * fromIntegral x) (10 + 20 * fromIntegral y) $ rectangleSolid 18 18
+    render (Unit (x, y)) = translate (10 + 20 * fromIntegral x) (10 + 20 * fromIntegral y) $ rectangleSolid 18 18
 
 instance Renderable Game where
-    render (Game _ snake) = pictures $ map render snake
+    render Begin = blank
+    render (Game _ _ _ food snake) = pictures $ (color red $ render food):(map (color green . render) snake)
