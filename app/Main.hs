514259bdc13da8ca4c20dccd60730060de6927e3
PistachioCake <rushil.udani@gmail.com>
2018-08-12 13:34:21 -0500

    Add documentation

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import System.Random
 import System.Exit (exitSuccess)
 import Lib
 
+-- | A helper function to easily build the original gamestate
 (<@>) :: (Applicative f) => f (a -> b) -> a -> f b
 f <@> a = f <*> pure a
 infixl 4 <@>
@@ -19,10 +20,12 @@ main = playIO window bkg fps initialState renderIO handleEvents update
         renderIO :: Game -> IO Picture
         renderIO g = return $ render g
 
-
+-- | The initial state of the game - will be updated to allow play
 initialState :: Game
 initialState = Begin
 
+-- | The update function, which steps the game to the next iteration.
+--   It uses the helper functions defined below.
 update :: Float -> Game -> IO Game
 update _ Begin = return Begin
 update _ (Lose {_score = score}) = return $ Lose score
@@ -31,15 +34,20 @@ update _ game =
     then return $ Lose $ (length $ _snake game) `div` 3 - 1
     else return . moveSnake . eatFood $ game
 
+-- | Moves the snake by adding a new unit to move the head, and
+--   removing a unit for the tail if appropriate. 
 moveSnake :: Game -> Game
 moveSnake game@(Game {_add = add, _vel = (velx, vely), _snake = snake}) = 
     let Unit (frontx, fronty) = head snake
     in game {_add = max 0 (add - 1), _snake = Unit (velx + frontx, vely + fronty):(if add > 0 then snake else init snake)}
 
+-- | Allows the snake to eat food if its head is directly on the food
 eatFood :: Game -> Game
 eatFood game@(Game {_add = add, _food = food, _snake = (front:_)}) = 
     if front == food then mkNextFood $ game {_add = add + 3} else game
 
+-- | Creates a new food unit using the StdGen in the Game. Shoule
+--   only be called when the snake has eaten the previous food. 
 mkNextFood :: Game -> Game
 mkNextFood game@(Game {_gen = gen, _snake = snake}) = 
     let (x, gen') = randomR boundX gen
@@ -47,6 +55,8 @@ mkNextFood game@(Game {_gen = gen, _snake = snake}) =
         food = Unit (x, y)
     in if food `elem` snake then mkNextFood $ game {_gen = gen''} else game {_gen = gen'', _food = food}
 
+-- | Checks if the game is lost, by checking whether the 
+--   snake has hit the wall or its tail. 
 hasLost :: Game -> Bool
 hasLost game@(Game {_snake = (front:tail)}) = wallCollision || tailCollision
     where
@@ -56,12 +66,18 @@ hasLost game@(Game {_snake = (front:tail)}) = wallCollision || tailCollision
             in notInRange boundX x || notInRange boundY y
         tailCollision = front `elem` tail
 
+-- | Handle keypresses by modifying the game state accordingly. 
 handleEvents :: Event -> Game -> IO Game
-handleEvents (EventKey (SpecialKey KeySpace) Down _ _) Begin = Game <$> getStdGen <@> 0 <@> up <@> Unit (0, 0) <@> [Unit (0, 0)]
-handleEvents (EventKey (Char 'r') Down _ _) (Lose s) = Game <$> getStdGen <@> 0 <@> up <@> Unit (0, 0) <@> [Unit (0, 0)]
+-- Change the direction of the snake.
 handleEvents (EventKey (SpecialKey KeyUp) Down _ _) game@(Game { }) = return game {_vel = up}
 handleEvents (EventKey (SpecialKey KeyDown) Down _ _) game@(Game { }) = return game {_vel = down}
 handleEvents (EventKey (SpecialKey KeyLeft) Down _ _) game@(Game { }) = return game {_vel = left}
 handleEvents (EventKey (SpecialKey KeyRight) Down _ _) game@(Game { }) = return game {_vel = right}
+-- Begin the game.
+handleEvents (EventKey (SpecialKey KeySpace) Down _ _) Begin = Game <$> getStdGen <@> 0 <@> up <@> Unit (0, 0) <@> [Unit (0, 0)]
+-- Reset the game.
+handleEvents (EventKey (Char 'r') Down _ _) (Lose s) = Game <$> getStdGen <@> 0 <@> up <@> Unit (0, 0) <@> [Unit (0, 0)]
+-- Quit the game.
 handleEvents (EventKey (Char 'q') Down _ _) _ = exitSuccess
+-- If all other patterns fail, do nothing. 
 handleEvents _ game = return game
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,40 +10,61 @@ import Graphics.Gloss.Data.Picture
 import Graphics.Gloss.Data.Color
 import System.Random
 
+-- | A datatype for representing anything that is on the board.
 newtype Unit = Unit (Int, Int) deriving (Eq, Show)
+-- | A type synonym to specify velocities.
 type Vel = (Int, Int)
-data Game = Begin | Game {_gen :: StdGen, _add :: Int, _vel :: Vel, _food :: Unit, _snake :: [Unit]} | Lose {_score :: Int}
+-- | The actual type of the game.
+data Game = Begin -- ^ The beginning, representing the intro screen.
+          | Game  -- ^ The actual, running game.
+            { _gen :: StdGen     -- ^ The generator for random numbers.
+            , _add :: Int        -- ^ The length of the snake yet to add.
+            , _vel :: Vel        -- ^ The direction the head of the snake is going in.
+            , _food :: Unit      -- ^ The position of the food.
+            , _snake :: [Unit]   -- ^ The position of all the units of the snake.
+            } 
+          | Lose {_score :: Int}
 
+-- | Constants that are used to manipulate the direction. 
 up, down, left, right :: Vel
 up = (0, 1)
 down = (0, -1)
 left = (-1, 0)
 right = (1, 0)
 
+-- | Constants that manipulate the images rendered.
 squareSize, drawSize :: Int
+-- | The size of each cell.
 squareSize = 20
+-- | The size of the colored part of each cell.
 drawSize = 18
 
+-- | The dimensions of the drawing.
 height, width :: Int
 height = 30
 width = 20
 
+-- | The boundaries of the board.
 boundX, boundY :: (Int, Int)
 boundX = (-height `div` 2, height `div` 2 - 1)
 boundY = (-width `div` 2, width `div` 2 - 1)
 
+-- | A typeclass for rendering data to pictures.
 class Renderable a where
-    render :: a -> Picture
+    render :: a -> Picture -- ^ The polymorphic render funciton.
 
 instance Renderable Unit where
     render (Unit (x, y)) = translate (fi $ squareSize `div` 2 + squareSize * x) (fi $ squareSize `div` 2 + squareSize * y) $ rectangleSolid (fi drawSize) (fi drawSize)
         where fi = fromIntegral
 
 instance Renderable Game where
+    -- | Display "Press space to begin." before the game beigns.
     render Begin = translate (-150) 0 $ scale 0.15 0.15 $ color white $ text "Press space to begin."
+    -- | Display the food and snake when the game is running.
     render (Game {_food = food, _snake = snake}) = pictures $ (color red $ render food):(map (color green . render) snake)
+    -- | Render some text when the game ends, including the score.
     render (Lose {_score = score}) = translate (-150) 0 $ color white $ pictures [
         translate 0 50 $ scale 0.3 0.3 $ text "Game Over!",
         scale 0.2 0.2 $ text $ "Your score was " ++ show score ++ ".", 
         translate 0 (-30) $ scale 0.15 0.15 $ text "Press R to play again.", 
-        translate 0 (-50) $ scale 0.15 0.15 $ text "Press Q to quit."]
+        translate 0 (-60) $ scale 0.15 0.15 $ text "Press Q to quit."]
